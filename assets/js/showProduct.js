802f1f5cb663f2773a28d50c9601e1aca0b96ae3
Bui Huu Dat <dat54261001@gmail.com>
2022-12-22 09:09:07 +0700

    update

---
+++ assets/js/showProduct.js
@@ -0,0 +1,55 @@
+let count = 1;
+const textShowProduct = $("#product-show-count");
+const btnShowProduct = $(".show-product-button button");
+
+$(btnShowProduct[0]).click(() => {
+  if (count <= 1) {
+    count = 1;
+  } else {
+    count -= 1;
+  }
+  $("#product-show-count").text(count);
+});
+$(btnShowProduct[1]).click(() => {
+  count += 1;
+  $("#product-show-count").text(count);
+});
+
+const showProductUser = $(".show-feedback-user");
+const productReview = $("#btn-showmore").text(
+  "Xem " + (showProductUser.length - 3) + " đánh giá khác"
+);
+const showMoreReview = $("#btn-showmore");
+
+showMoreReview.click(() => {
+  if ($(showProductUser[3]).hasClass("hidden")) {
+    showProductUser.removeClass("hidden");
+    productReview.text("Ẩn bớt");
+    flag = 0;
+  } else {
+    productReview.text(
+      "Xem " + (showProductUser.length - 3) + " đánh giá khác"
+    );
+    showProductUser.map((i, e) => {
+      if (i > 2) {
+        $(showProductUser[i]).addClass("hidden");
+      }
+    });
+  }
+});
+showProductUser.map((i, e) => {
+  if (i > 2) {
+    $(showProductUser[i]).addClass("hidden");
+  }
+});
+
+const writeReview = $("#btn-writefb");
+const formReview = $(".form-reviews");
+const cancelForm = $(".cancelForm");
+writeReview.click(() => {
+  formReview.removeClass("hidden");
+});
+
+cancelForm.click(() => {
+  formReview.addClass("hidden");
+});
